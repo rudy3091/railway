9dc0ab2e5cf33e1fac5bdde3719db4bd0ebff1f5
rudy3091 <grayblack313@gmail.com>
2022-10-16 19:53:02 +0900

    Add loop functions

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,10 @@
-import { SingledAsyncRailway, SingledRailway } from './types';
+import { success, successPromisify } from './helper';
+import {
+  DoubledAsyncRailway,
+  DoubledRailway,
+  SingledAsyncRailway,
+  SingledRailway,
+} from './types';
 
 export const convertAsync = <T>(
   railway: SingledRailway<T, T>
@@ -10,4 +16,28 @@ export const convertAsync = <T>(
 
 export const pipe = <T>(...fns: Array<(arg: T) => T>) => {
   return (value: T) => fns.reduce((acc, fn) => fn(acc), value);
+};
+
+export const loopWhileSuccess = <T>(railway: DoubledRailway<T, T>) => {
+  return (value: T) => {
+    let result = railway(success(value));
+    while (result.success) result = railway(success(result.value));
+  };
+};
+
+export const loopAsync = <T>(railway: DoubledAsyncRailway<T, T>) => {
+  return async (value: T) => {
+    let result = await railway(successPromisify(value));
+    while (true) {
+      if (result.success) result = await railway(successPromisify(result.value));
+      else result = await railway(successPromisify(value));
+    }
+  };
+};
+
+export const loopAsyncWhileSuccess = <T>(railway: DoubledAsyncRailway<T, T>) => {
+  return async (value: T) => {
+    let result = await railway(successPromisify(value));
+    while (result.success) result = await railway(successPromisify(value));
+  };
 };
